23f65663535f9cee2a9e2e709da767606c04bd07
ngron <syumuu1219@outlook.jp>
2018-06-03 20:41:01 +0900

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   def hello
-    render hetml: "hello,world"
+    redirect_to "/static_pages/home"
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
 class StaticPagesController < ApplicationController
+  def new
+  end
+
   def home
   end
 
   def help
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
